e3ef58f893a0b9a8c823079cd55e287dc3434f11
BluSteve <steve.scyr@gmail.com>
2026-01-02 15:05:45 +0200

    Clean up to make it stream a massive circuit to disk

--- streaming.py
+++ streaming.py
@@ -1,6 +1,9 @@
 import csv
 import random
 from time import time
+from typing import List
+
+from scipy.constants import gibi
 
 from tqec import BlockGraph
 from tqec.circuit.qubit import GridQubit
@@ -41,10 +44,15 @@ def memory(nx: int, ny: int, t: int) -> LayerTree:
 
     r = random.Random(100)
 
+    graphs: List[BlockGraph | None] = [None] * t
+
+    graphs[0] = BlockGraph()
     # Create n×n grid at each time slice
     for x in range(nx):
         for y in range(ny):
             graph.add_cube(Position3D(x, y, 0), r.choice(cd))
+            graphs[0].add_cube(Position3D(x, y, 0), r.choice(cd))
+
 
     # Add spatial connections within the first layer
     for x in range(nx):
@@ -52,24 +60,30 @@ def memory(nx: int, ny: int, t: int) -> LayerTree:
             try:
                 if x < nx - 1:
                     graph.add_pipe(Position3D(x, y, 0), Position3D(x + 1, y, 0))
+                    graphs[0].add_pipe(Position3D(x, y, 0), Position3D(x + 1, y, 0))
             except TQECError:
                 pass
 
             try:
                 if y < ny - 1:
                     graph.add_pipe(Position3D(x, y, 0), Position3D(x, y + 1, 0))
+                    graphs[0].add_pipe(Position3D(x, y, 0), Position3D(x, y + 1, 0))
             except TQECError:
                 pass
 
     # Add temporal layers and connections
     for i in range(1, t):
+        gi = graphs[0] if i < 5 else BlockGraph()
+
         # Add all cubes in the n×n grid at time slice i
         for x in range(nx):
             for y in range(ny):
                 graph.add_cube(Position3D(x, y, i), r.choice(cd))
+                gi.add_cube(Position3D(x, y, i), r.choice(cd))
                 # Add temporal pipe from previous time slice
                 try:
                     graph.add_pipe(Position3D(x, y, i - 1), Position3D(x, y, i))
+                    gi.add_pipe(Position3D(x, y, i - 1), Position3D(x, y, i))
                 except TQECError:
                     pass
 
@@ -80,20 +94,24 @@ def memory(nx: int, ny: int, t: int) -> LayerTree:
                     try:
                         if x < nx - 1:
                             graph.add_pipe(Position3D(x, y, i), Position3D(x + 1, y, i))
+                            gi.add_pipe(Position3D(x, y, i), Position3D(x + 1, y, i))
                     except TQECError:
                         pass
 
                     try:
                         if y < ny - 1:
                             graph.add_pipe(Position3D(x, y, i), Position3D(x, y + 1, i))
+                            gi.add_pipe(Position3D(x, y, i), Position3D(x, y + 1, i))
                     except TQECError:
                         pass
 
     # graph.view_as_html(f"memory_nx{nx}_ny{ny}_t{t}.html")
 
+    graphs[0].view_as_html(f'memory_nx{nx}_ny{ny}_t{t}_layer0.html')
+
     compiled_graph = compile_block_graph(graph, observables="auto")
 
-    return compiled_graph.to_layer_tree()
+    return compiled_graph.to_layer_tree(), compile_block_graph(graphs[0]).to_layer_tree()
 
 
 def benchmark():
@@ -147,9 +165,9 @@ def benchmark():
 
 if __name__ == "__main__":
     nx = 3
-    ny = 4
-    t = 200
-    k = 3
+    ny = 3
+    t = 10
+    k = 1
 
     # lt2 = memory(nx,ny, t)
     #
@@ -167,12 +185,12 @@ if __name__ == "__main__":
     magic_qm = generate_qubit_map(nx, ny, k)
 
     start = time()
-    lt = memory(nx, ny, t)
+    lt, lt0 = memory(nx, ny, t)
     end = time()
     print(f"Layer tree generation time: {end - start}\n\n")
 
     start = time()
-    citer = lt.generate_circuit_stream(k, magic_qm)
+    citer = lt0.generate_circuit_stream(k, magic_qm)
     end = time()
     print(f"Streamed annotations generation time: {end - start}\n\n")
 
@@ -185,7 +203,7 @@ if __name__ == "__main__":
             print(f"{i}, {time() - last}, {circ.__str__()[:20].replace('\n', ' ')}")
             last = time()
             i += 1
-            f.write(str(circ))
+            f.write(str(circ) + "\n")
 
     end = time()
     print(f"Streamed circuit generation time: {end - start}\n\n")
