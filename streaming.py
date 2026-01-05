f9615bf81ee419a4075714a43dbb409605d4fcf4
BluSteve <steve.scyr@gmail.com>
2026-01-05 10:48:06 +0200

    New streaming.py

--- streaming.py
+++ streaming.py
@@ -1,20 +1,17 @@
-import csv
 import random
 from time import time
-from typing import List
 
-from scipy.constants import gibi
+import stim
 
 from tqec import BlockGraph
 from tqec.circuit.qubit import GridQubit
 from tqec.circuit.qubit_map import QubitMap
 from tqec.compile.compile import compile_block_graph
-from tqec.compile.tree.tree import LayerTree
 from tqec.utils import TQECError
 from tqec.utils.position import Position3D
 
 
-def generate_qubit_map(x, y, k):
+def _generate_qubit_map(x, y, k):
     d = 2 * k + 1
     a = 2 * d
 
@@ -36,23 +33,17 @@ def generate_qubit_map(x, y, k):
     return qm
 
 
-def memory(nx: int, ny: int, t: int) -> LayerTree:
+def _random_block_graph(nx: int, ny: int, t: int) -> BlockGraph:
     graph = BlockGraph()
 
-    # cd = ['ZXX', 'ZXZ', 'XZX', 'XZZ']
     cd = ["ZXX", "ZXZ"]
 
     r = random.Random(100)
 
-    graphs: List[BlockGraph | None] = [None] * t
-
-    graphs[0] = BlockGraph()
-    # Create n×n grid at each time slice
+    # Create nxn grid at each time slice
     for x in range(nx):
         for y in range(ny):
             graph.add_cube(Position3D(x, y, 0), r.choice(cd))
-            graphs[0].add_cube(Position3D(x, y, 0), r.choice(cd))
-
 
     # Add spatial connections within the first layer
     for x in range(nx):
@@ -60,30 +51,24 @@ def memory(nx: int, ny: int, t: int) -> LayerTree:
             try:
                 if x < nx - 1:
                     graph.add_pipe(Position3D(x, y, 0), Position3D(x + 1, y, 0))
-                    graphs[0].add_pipe(Position3D(x, y, 0), Position3D(x + 1, y, 0))
             except TQECError:
                 pass
 
             try:
                 if y < ny - 1:
                     graph.add_pipe(Position3D(x, y, 0), Position3D(x, y + 1, 0))
-                    graphs[0].add_pipe(Position3D(x, y, 0), Position3D(x, y + 1, 0))
             except TQECError:
                 pass
 
     # Add temporal layers and connections
     for i in range(1, t):
-        gi = graphs[0] if i < 5 else BlockGraph()
-
-        # Add all cubes in the n×n grid at time slice i
+        # Add all cubes in the nxn grid at time slice i
         for x in range(nx):
             for y in range(ny):
                 graph.add_cube(Position3D(x, y, i), r.choice(cd))
-                gi.add_cube(Position3D(x, y, i), r.choice(cd))
                 # Add temporal pipe from previous time slice
                 try:
                     graph.add_pipe(Position3D(x, y, i - 1), Position3D(x, y, i))
-                    gi.add_pipe(Position3D(x, y, i - 1), Position3D(x, y, i))
                 except TQECError:
                     pass
 
@@ -94,120 +79,100 @@ def memory(nx: int, ny: int, t: int) -> LayerTree:
                     try:
                         if x < nx - 1:
                             graph.add_pipe(Position3D(x, y, i), Position3D(x + 1, y, i))
-                            gi.add_pipe(Position3D(x, y, i), Position3D(x + 1, y, i))
                     except TQECError:
                         pass
 
                     try:
                         if y < ny - 1:
                             graph.add_pipe(Position3D(x, y, i), Position3D(x, y + 1, i))
-                            gi.add_pipe(Position3D(x, y, i), Position3D(x, y + 1, i))
                     except TQECError:
                         pass
 
-    # graph.view_as_html(f"memory_nx{nx}_ny{ny}_t{t}.html")
-
-    graphs[0].view_as_html(f'memory_nx{nx}_ny{ny}_t{t}_layer0.html')
-
-    compiled_graph = compile_block_graph(graph, observables="auto")
-
-    return compiled_graph.to_layer_tree(), compile_block_graph(graphs[0]).to_layer_tree()
+    return graph
 
 
-def benchmark():
-    with open("results.csv", "a", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        # writer.writerow(["n", "k", "duration1", "duration2"])
+def benchmark_stream(nx: int, ny: int, t: int, k: int, compare_to_unstreamed: bool = False) -> None:
+    """Benchmark streaming generation of quantum circuits for a compiled block graph.
 
-        for x in range(3, 20):
-            start = time()
+    Generates a block graph, compiles it into a layer tree, and produces circuit
+    instructions via streaming. Optionally compares streamed results with unstreamed
+    generation. Logs timing metrics for performance analysis.
 
-            lt = memory(x, 3)
+    NOTE: The qubit map used for streaming is different from the one used for unstreamed,
+    even though the resulting circuit is functionally the same. To prove the unstreamed
+    and streamed circuits are exactly the same, we need to use the same qubit map for both.
+    This means piping the qubit map from the unstreamed generation into the streamed generation.
 
-            end = time()
+    Args:
+        nx: Number of blocks along the x-axis in the block graph.
+        ny: Number of blocks along the y-axis in the block graph.
+        t: Time parameter for generating the block graph.
+        k: Number of layers to use for circuit generation in the layer tree.
+        compare_to_unstreamed: Whether to compare the streamed circuit with an
+            unstreamed circuit. Defaults to False.
 
-            duration0 = end - start
+    """
+    print(f"Benchmarking streaming with nx={nx}, ny={ny}, t={t}, k={k}\n")
 
-            print(f"{x} {duration0}")
+    start = time()
+    block_graph = _random_block_graph(nx, ny, t)
+    compiled_graph = compile_block_graph(block_graph, observables="auto")
+    lt = compiled_graph.to_layer_tree()
+    end = time()
+    print(f"Layer tree generation time (s): {end - start}\n")
 
-            # for k in range(7, 8):
-            #     citer = lt.generate_circuit_stream(k)
-            #     master_circuit = stim.Circuit()
-            #
-            #     start = time()
-            #
-            #     next(citer)  # annotations generation finish here
-            #
-            #     end = time()
-            #
-            #     duration1 = end - start
-            #
-            #     start = time()
-            #
-            #     i = 0
-            #     for circ in citer:
-            #         master_circuit += circ
-            #         # print(circ)
-            #         # print('----------------------------')
-            #         i += 1
-            #         # if i == 10:
-            #         #     break
-            #
-            #     # with open(f"master_circuit_n{x}_k{k}.stim", "w") as f:
-            #     #     f.write(str(master_circuit))
-            #
-            #     end = time()
-            #     duration2 = end - start
-            #     # writer.writerow([x, k, duration1, duration2])
-            #
-            #     print(f"{x} {k} {duration0} {duration1} {duration2}")
+    circuit = None
+    if compare_to_unstreamed:
+        lt2 = compiled_graph.to_layer_tree()  # a copy is needed here
+
+        start = time()
+        circuit = lt2.generate_circuit(k)
+        end = time()
+        print(f"Unstreamed circuit generation time (s): {end - start}\n")
+
+        magic_qm = lt2._get_global_qubit_map(k)
+    else:
+        magic_qm = _generate_qubit_map(
+            nx, ny, k
+        )  # This qubit map is not tight on the qubits needed.
+
+    start = time()
+    citer = lt.generate_circuit_stream(k, magic_qm)
+    end = time()
+    print(f"Initial stream generation time (s): {end - start}\n")
+
+    start = time()
+
+    last = time()
+    master_circuit = stim.Circuit() if compare_to_unstreamed else None
+    with open("master_circuit.txt", "w+") as f:
+        print("Index, Time Taken (s), Circuit Snippet")
+        i = 0
+        for circ in citer:
+            print(f"{i}, {time() - last}, " + circ.__str__()[:20].replace("\n", " "))
+            last = time()
+            i += 1
+            f.write(str(circ))
+            if compare_to_unstreamed:
+                master_circuit += circ
+
+    end = time()
+    print(f"Total streamed circuit generation time (s): {end - start}\n")
+
+    if compare_to_unstreamed:
+        print("Comparing streamed vs unstreamed circuits...")
+        same = master_circuit == circuit
+        if same:
+            print("Circuits are the same!")
+        else:
+            print("Circuits are different!")
+        assert same
 
 
 if __name__ == "__main__":
     nx = 3
     ny = 3
     t = 10
-    k = 1
+    k = 2
 
-    # lt2 = memory(nx,ny, t)
-    #
-    # start = time()
-    # circuit = lt2.generate_circuit(k)
-    # end = time()
-    #
-    # print(f"Single circuit generation time: {end - start}\n\n")
-    #
-    # with open("circuit.txt", "w") as f:
-    #     f.write(str(circuit))
-    #
-    # print('starting for real\n\n')
-
-    magic_qm = generate_qubit_map(nx, ny, k)
-
-    start = time()
-    lt, lt0 = memory(nx, ny, t)
-    end = time()
-    print(f"Layer tree generation time: {end - start}\n\n")
-
-    start = time()
-    citer = lt0.generate_circuit_stream(k, magic_qm)
-    end = time()
-    print(f"Streamed annotations generation time: {end - start}\n\n")
-
-    start = time()
-    i = 0
-
-    last = time()
-    with open("master_circuit.txt", "w+") as f:
-        for circ in citer:
-            print(f"{i}, {time() - last}, {circ.__str__()[:20].replace('\n', ' ')}")
-            last = time()
-            i += 1
-            f.write(str(circ) + "\n")
-
-    end = time()
-    print(f"Streamed circuit generation time: {end - start}\n\n")
-
-    # print(circuit == master_circuit)
-
-    print(lt)
+    benchmark_stream(nx, ny, t, k, compare_to_unstreamed=True)
